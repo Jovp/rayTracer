e8355d59f2892a3ad8b51377fc0837dba30ca777
lexoux <alexandre.roux5@gmail.com>
2015-04-15 22:13:02 +0200

    Undo Merge

--- build/projet3D.build/Debug/projet3D.build/Objects-normal/x86_64/main.d
+++ build/projet3D.build/Debug/projet3D.build/Objects-normal/x86_64/main.d
@@ -1,6 +1,16 @@
+<<<<<<< HEAD
 dependencies: /Users/alexandre/rayTracer/projet3D/main.cpp \
   /Users/alexandre/rayTracer/projet3D/Vec3.h \
   /Users/alexandre/rayTracer/projet3D/tiny_obj_loader.h \
   /Users/alexandre/rayTracer/projet3D/kdTree.h \
   /Users/alexandre/rayTracer/projet3D/BBox.h \
   /Users/alexandre/rayTracer/projet3D/Ray.h
+=======
+dependencies: \
+  /Users/Julien/Desktop/TELECOM/Telecom\ Travail/2A/3D/my_ray_tracer/projet3D/projet3D/main.cpp \
+  /Users/Julien/Desktop/TELECOM/Telecom\ Travail/2A/3D/my_ray_tracer/projet3D/projet3D/Vec3.h \
+  /Users/Julien/Desktop/TELECOM/Telecom\ Travail/2A/3D/my_ray_tracer/projet3D/projet3D/tiny_obj_loader.h \
+  /Users/Julien/Desktop/TELECOM/Telecom\ Travail/2A/3D/my_ray_tracer/projet3D/projet3D/kdTree.h \
+  /Users/Julien/Desktop/TELECOM/Telecom\ Travail/2A/3D/my_ray_tracer/projet3D/projet3D/BBox.h \
+  /Users/Julien/Desktop/TELECOM/Telecom\ Travail/2A/3D/my_ray_tracer/projet3D/projet3D/Ray.h
+>>>>>>> Julien2
